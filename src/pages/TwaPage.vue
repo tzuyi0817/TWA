1abab762b57f5a79d7ea9b7ad5a34ba6bbf556b4
tzuyi <a0988426059@gmail.com>
2023-11-01 13:26:45 +0800

    chore: telegram web app initial data test

--- src/pages/TwaPage.vue
+++ src/pages/TwaPage.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import WebApp from '@twa-dev/sdk';
 import { showBackButton, hideBackButton, showMainButton, hideMainButton } from '@/plugins/twa';
+
+const initData = ref('');
+const initDataUnsafe = ref();
 
 function showWebAppAlert() {
   WebApp.showAlert('Main Button was clicked');
 }
 
+function setInitData() {
+  initData.value = WebApp.initData;
+  initDataUnsafe.value = WebApp.initDataUnsafe;
+}
+
 onMounted(() => {
   showBackButton();
   showMainButton(showWebAppAlert);
+  setInitData();
 });
 onBeforeUnmount(() => {
   hideBackButton();
@@ -17,4 +26,16 @@ onBeforeUnmount(() => {
 });
 </script>
 
-<template>TWA</template>
+<template>
+  <h1>TWA</h1>
+
+  <article>
+    <p>initData</p>
+    {{ initData }}
+  </article>
+
+  <article>
+    <p>initDataUnsafe</p>
+    {{ initDataUnsafe }}
+  </article>
+</template>
