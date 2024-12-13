437f869dbc473db83d641e4bb388bd6f9f185fdd
tzuyi <a0988426059@gmail.com>
2024-12-13 14:31:51 +0800

    chore: build command remove typeschek directive

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -25,9 +25,7 @@ import HelloWorld from '@/components/indexPage/HelloWorld.vue';
     </a>
   </div>
   <hello-world msg="Vite + Vue" />
-  <router-link to="/twa">
-    TWA
-  </router-link>
+  <router-link to="/twa"> TWA </router-link>
 </template>
 
 <style lang="postcss" scoped>
